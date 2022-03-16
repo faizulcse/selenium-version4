0e77a50eefaf3a3209a81fd1895111bedbb5725a
faizul <faizulcse@gmail.com>
2022-03-16 21:42:51 +0600

    remove selenium server

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -6,7 +6,6 @@ import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.time.Duration;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,14 +16,12 @@ public class MainTest {
     public void setUp() {
         Logger.getLogger("org.openqa.selenium").setLevel(Level.OFF);
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox");
         try {
             driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-            driver.get("https://google.com");
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
+
     }
 
     @AfterMethod
